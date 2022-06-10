a7847e59c8c4f264b6b8060b3e5e4c172275fa58
Drelf2018 <41439182+Drelf2018@users.noreply.github.com>
2022-06-10 11:54:43 +0800

    Update notice.py

--- notice.py
+++ notice.py
@@ -1,12 +1,47 @@
 from pywebio.output import *
-from PIL import Image
+from PIL import Image, ImageDraw
 import os
+
+
+def circle_corner(img, radii=0):  # 把原图片变成圆角，这个函数是从网上找的
+    """
+    圆角处理
+    :param img: 源图象。
+    :param radii: 半径，如：30。
+    :return: 返回一个圆角处理后的图象。
+    """
+    if radii == 0:
+        radii = int(0.1*img.height)
+    else:
+        radii = int(radii)
+    # 画圆（用于分离4个角）
+    circle = Image.new('L', (radii * 2, radii * 2), 0)  # 创建一个黑色背景的画布
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, radii * 2, radii * 2), fill=255)  # 画白色圆形
+
+    # 原图
+    img = img.convert("RGBA")
+    w, h = img.size
+
+    # 画4个角（将整圆分离为4个部分）
+    alpha = Image.new('L', img.size, 255)
+    alpha.paste(circle.crop((0, 0, radii, radii)), (0, 0))  # 左上角
+    alpha.paste(circle.crop((radii, 0, radii * 2, radii)), (w - radii, 0))  # 右上角
+    alpha.paste(circle.crop((radii, radii, radii * 2, radii * 2)), (w - radii, h - radii))  # 右下角
+    alpha.paste(circle.crop((0, radii, radii, radii * 2)), (0, h - radii))  # 左下角
+
+    img.putalpha(alpha)  # 白色区域透明可见，黑色区域不可见
+    
+    image = Image.new('RGB', size=(w, h), color=(255, 255, 255))
+    image.paste(img, (0, 0), mask=img)
+
+    return image
 
 
 PICS = {}
 for root, folders, files in os.walk('./notice_pic'):
     for file in files:
-        PICS[file] = Image.open(os.path.join(root,file))
+        PICS[file] = circle_corner(Image.open(os.path.join(root,file)), 50)
 
 
 def notice():
